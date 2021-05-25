f25d33008dcc19879e1fd76ddef5fdd7e123cd64
Omer Oreg <sarhatabaot1@gmail.com>
2021-05-25 10:42:19 +0300

    Refactored reload command

--- src/main/java/me/messageofdeath/commandnpc/commands/ReloadCommand.java
+++ src/main/java/me/messageofdeath/commandnpc/commands/ReloadCommand.java
@@ -10,32 +10,36 @@ import me.messageofdeath.commandnpc.CommandNPC;
 import me.messageofdeath.commandnpc.Database.LanguageSettings.LanguageSettings;
 import me.messageofdeath.commandnpc.Database.PluginSettings.PluginSettings;
 import me.messageofdeath.commandnpc.Utilities.BungeeCord.BungeeCordUtil;
+import org.jetbrains.annotations.NotNull;
 
 public class ReloadCommand implements CommandExecutor {
 
-	@Override
-	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		if(args.length == 1) {
-			if (args[0].equalsIgnoreCase("reload")) {
-				if(sender.hasPermission("commandnpc.admin") || sender.isOp()) {
-					if(PluginSettings.BungeeCord.getBoolean()) {
-						BungeeCordUtil.disableUtil();
-					}
-					CommandNPC.getInstance().reloadConfigX();
-					Messaging.send(sender, LanguageSettings.Commands_CmdNPC_Reload.getSetting());
-					if(PluginSettings.BungeeCord.getBoolean()) {
-						BungeeCordUtil.setupUtil();
-					}
-					return true;
-				}else{
-					Messaging.sendError(sender, LanguageSettings.Commands_NoPermission.getSetting());
-				}
-			}else{
-				Messaging.sendError(sender, LanguageSettings.Commands_WrongArgs.getSetting());
-			}
-		}else{
-			Messaging.sendError(sender, LanguageSettings.Commands_WrongArgs.getSetting());
-		}
-		return false;
-	}
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command cmd, @NotNull String label, String[] args) {
+        if (args.length != 1) {
+            Messaging.sendError(sender, LanguageSettings.Commands_WrongArgs.getSetting());
+            return false;
+        }
+
+        if (!args[0].equalsIgnoreCase("reload")) {
+            Messaging.sendError(sender, LanguageSettings.Commands_WrongArgs.getSetting());
+            return false;
+        }
+
+        if (!sender.hasPermission("commandnpc.admin") && !sender.isOp()) {
+            Messaging.sendError(sender, LanguageSettings.Commands_NoPermission.getSetting());
+            return false;
+        }
+
+        if (PluginSettings.BungeeCord.getBoolean()) {
+            BungeeCordUtil.disableUtil();
+        }
+
+        CommandNPC.getInstance().reloadConfigX();
+        Messaging.send(sender, LanguageSettings.Commands_CmdNPC_Reload.getSetting());
+        if (PluginSettings.BungeeCord.getBoolean()) {
+            BungeeCordUtil.setupUtil();
+        }
+        return true;
+    }
 }
